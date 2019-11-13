6542b0ab72fb1e4b55874cd8eea9e2f0722e9c33
Thesharing <cyprestar@outlook.com>
2019-11-13 18:57:28 +0800

    Improve Dict and TextDict --> spider-util 0.1.5

--- spiderutil/structure/dict.py
+++ spiderutil/structure/dict.py
@@ -1,24 +1,51 @@
-class Dict:
-    def __init__(self, structure=None, **kwargs):
-        if type(structure) is dict:
-            for k, v in kwargs:
-                structure[k] = v
-        self.__initial__ = structure
+import json
 
-    def __new__(cls, structure=None, **kwargs):
+from io import IOBase
+
+
+class Dict(dict):
+    def __init__(self, source=None, **kwargs):
+        super(Dict, self).__init__()
+        for k, v in kwargs.items():
+            if k not in self:
+                self[k] = v
+
+    def __new__(cls, source=None, **kwargs):
         self = super(Dict, cls).__new__(cls)
-        self.__init__(structure=structure, **kwargs)
-        if type(self.__initial__) is dict:
-            self.__dict__ = {key: Dict(self.__initial__[key]) for key in self.__initial__}
-        elif type(self.__initial__) is list:
-            self = [Dict(item) for item in self.__initial__]
+        self.__init__(source=source, **kwargs)
+        if isinstance(source, dict):
+            for k, v in source.items():
+                self[k] = Dict(v)
+        elif isinstance(source, list):
+            self = [Dict(item) for item in source]
         else:
-            self = self.__initial__
+            self = source
         return self
 
-    def __str__(self):
-        return str(self.dict)
+    def __getattr__(self, attr):
+        try:
+            return self[attr]
+        except KeyError:
+            raise AttributeError("The dict has no attribute '%s'" % attr)
 
-    @property
-    def dict(self):
-        return {k: v for k, v in self.__dict__.items() if k[0] != '_'}
+    def __setattr__(self, attr, value):
+        self[attr] = value
+
+
+class TextDict(dict):
+
+    @staticmethod
+    def load(source):
+        if isinstance(source, IOBase):
+            return json.load(source, object_hook=lambda pairs: TextDict(pairs.items()))
+        elif isinstance(source, str):
+            return json.loads(source, object_hook=lambda pairs: TextDict(pairs.items()))
+
+    def __getattr__(self, attr):
+        try:
+            return self[attr]
+        except KeyError:
+            raise AttributeError("The dict has no attribute '%s'" % attr)
+
+    def __setattr__(self, attr, value):
+        self[attr] = value
